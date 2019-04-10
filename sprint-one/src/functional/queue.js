167f3c16ac22684217e0f9d5f1aa07bc0704b052
shawn-chambers <chambers.shawn.89@gmail.com>
2019-04-10 14:47:12 -0700

    Passes all tests but 1 for functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,23 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size ++;
   };
 
   someInstance.dequeue = function() {
+    size --
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      return 0;
+    }
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //storage stores 'size' number of keys and items as their values?
+  //need way to add size numbers and subsequent items to storage.properties
+
+  //storatge[size] = whatever we're storing here
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size ++;
   };
 
   someInstance.pop = function() {
+    size --;
   };
 
   someInstance.size = function() {
+    if (size <= 0) {
+      return 0;
+    }
+    return size;
   };
 
   return someInstance;
