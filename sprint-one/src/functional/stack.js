93a26e815c139eb2dbe2f4b1c0a3ec136581c8d8
shawn-chambers <chambers.shawn.89@gmail.com>
2019-04-10 15:36:00 -0700

    Passed all stack test

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,23 +6,41 @@ var Stack = function() {
   //storage stores 'size' number of keys and items as their values?
   //need way to add size numbers and subsequent items to storage.properties
 
+  // var obj = {};
+
+  // for (var i = 0; i < 4; i ++) {
+  //   obj[i] = i;
+  // }
+  //   console.log(obj);
+
+  //   {0: 0, 1: 1, 2: 2, 3: 3}
+
+
+  // Stack = {0: data,
+  //         1: moreData
+  //   };
+
   //storatge[size] = whatever we're storing here
-  var size = 0;
+  var top = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    size ++;
+    storage[top] = value;
+    top ++;
   };
 
   someInstance.pop = function() {
-    size --;
+    top --;
+    var result = storage[top];
+    delete storage[top];
+    return result;
   };
 
   someInstance.size = function() {
-    if (size <= 0) {
+    if (top <= 0) {
       return 0;
     }
-    return size;
+    return top;
   };
 
   return someInstance;
