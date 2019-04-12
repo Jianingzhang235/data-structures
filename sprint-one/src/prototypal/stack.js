a7a349a8c54a35aea4b4170ec0e3a13d27011e0f
shawn-chambers <chambers.shawn.89@gmail.com>
2019-04-12 11:19:02 -0700

    Stack prototypal looks good but fails 2 tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj = Object.create(stackMethods);
+  return obj;
 };
-
 var stackMethods = {};
+var storage = {};
+var size = 0;
 
+stackMethods.push = function(value) {
+  storage[size] = value;
+  size ++;
+};
+stackMethods.pop = function() {
+    size --;
+    var result = storage[size];
+    delete storage[size];
+    return result;
+};
+stackMethods.size = function() {
+   if (size <= 0) {
 
+      return 0;
+    }
+    return size;
+};
