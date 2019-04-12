78b9f65db392f02c263eca39ec50757f628b2080
shawn-chambers <chambers.shawn.89@gmail.com>
2019-04-12 12:12:56 -0700

    Fixed stack prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,10 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var instance = {};
-  instance = Object.create(stackMethods);
+
+  var instance = Object.create(stackMethods);
   instance.storage = {};
-  instance.top = {};
+  instance.top = 0;
   return instance;
 };
 var stackMethods = {};
