21c75395314d9d56e4b4fd7bdca172feaa1fcb91
shawn-chambers <chambers.shawn.89@gmail.com>
2019-04-13 10:07:17 -0700

    Finished tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,54 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  //children is an array;
+    // fix me
 
   return newTree;
 };
 
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+}
+
 var treeMethods = {};
 
+//The Object.assign(target, source) method is used to copy the values of all enumerable own properties from one or more source objects to a target object. It will return the target object
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+
+
+  // extend(newTree, treeMethods);
+  this.children.push(child);
+
 };
 
 treeMethods.contains = function(target) {
+  //travels over whole tree
+    //if it finds target return true
+  var targetChild = Tree(target);
+  var ifFound = false;
+  function travel (targetChild) {
+    if (targetChild.value === target) {
+      ifFound = true;
+    }
+    targetChild.children.forEach(function(child) {
+      travel(child);
+    })
+  }
+  travel (this);
+  return ifFound;
+
+
+
+
 };
 
 
